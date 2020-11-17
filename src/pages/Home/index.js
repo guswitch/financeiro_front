bcf1e1b6817c0a9ae260dfb2d8a0ed93aa5294bb
guswitch <gustaavohenrique@hotmail.com>
2020-11-16 22:06:31 -0300

    added div to home fragment

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,7 +12,7 @@ import Cotacao from '../../components/Cotacao';
 export default function Home() {
 
     return (
-        <>
+        <div>
             <Header />
 
             <div id="container-root">
@@ -29,7 +29,7 @@ export default function Home() {
                 <div> This is being built </div>
             </div> */}
 
-        </>
+        </div>
 
     )
 }
