3b6a894d2c8019dc45a977cb68bdaa23539a7a8f
guswitch <gustaavohenrique@hotmail.com>
2020-11-16 22:27:10 -0300

    changed route home

--- src/routes.js
+++ src/routes.js
@@ -28,7 +28,7 @@ const Routes = () => (
             <Route exact path='/auth' component={(Login)}/>
             <Route path='/signup' component={(Register)}/>
             <Route path='/forgetpassword' component={(ForgetPass)}/>
-            <PrivateRoute path='/' component={Home}/>
+            <PrivateRoute path='/home' component={Home}/>
             <PrivateRoute path='/profile' component={Profile}/>
         </Switch>
     </BrowserRouter>
